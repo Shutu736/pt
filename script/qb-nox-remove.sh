92dc834d700275127b2b3171af7902b592d64831
shutu <996822362@qq.com>
2022-01-22 05:24:50 +0800

    修改顺序

--- script/qb-nox-remove.sh
+++ script/qb-nox-remove.sh
@@ -9,16 +9,6 @@ username=$1
 
 #判断文件是否存在
 if [[ ! -f "/usr/bin/qb-nox-static-438-lt1214" ]]; then
-  # 4.3.8
-  echo -e "\033[36m ================= 删除qb-nox 4.3.8 ================= \033[0m"
-  systemctl stop qb-nox-static-438-lt1214@$username
-  rm -rf /etc/systemd/system/qb-nox-static-438-lt1214@.service
-  rm -rf /usr/bin/qb-nox-static-438-lt1214
-  systemctl disable qb-nox-static-438-lt1214@$username
-  rm -rf /home/$username/.config/qBittorrent/
-  rm -rf /home/$username/.local/share/data/qBittorrent/
-  rm -rf /home/$username/.cache/qBittorrent/
-else
   # 4.1.9
   echo -e "\033[36m ================= 删除qb-nox 4.1.9 ================= \033[0m"
   systemctl stop qb-nox-static-419-lt1114@$username
@@ -28,4 +18,14 @@ else
   rm -rf /home/$username/.config/qBittorrent/
   rm -rf /home/$username/.local/share/data/qBittorrent/
   rm -rf /home/$username/.cache/qBittorrent
+else
+  # 4.3.8
+  echo -e "\033[36m ================= 删除qb-nox 4.3.8 ================= \033[0m"
+  systemctl stop qb-nox-static-438-lt1214@$username
+  rm -rf /etc/systemd/system/qb-nox-static-438-lt1214@.service
+  rm -rf /usr/bin/qb-nox-static-438-lt1214
+  systemctl disable qb-nox-static-438-lt1214@$username
+  rm -rf /home/$username/.config/qBittorrent/
+  rm -rf /home/$username/.local/share/data/qBittorrent/
+  rm -rf /home/$username/.cache/qBittorrent/
 fi
