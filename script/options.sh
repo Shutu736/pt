fcb250e59d8d5a6409096a9a892fed3f3f816407
shutu <996822362@qq.com>
2022-01-23 21:34:57 +0800

    增加dns_secret

--- script/options.sh
+++ script/options.sh
@@ -5,7 +5,7 @@
 # Date:                 2022-1-23
 # Description:          参数处理脚本
 
-args=`getopt -o u:p:d:t:i:k: -al username:,password:,domain:,type:,id:,key: -n 'seedbox.sh' -- "$@"`
+args=`getopt -o u:p:d:t:i:k:s: -al username:,password:,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
 eval set -- "$args"
 
 while [ -n "$1" ]
@@ -14,9 +14,10 @@ do
     -u|--username) username=$2; shift 2;;
     -p|--password) password=$2; shift 2;;
     -d|--domain) domain=$2; shift 2;;
-    -t|--type) dns_type=$2; shift 2;;
-    -i|--id) dns_id=$2; shift 2;;
-    -k|--key) dns_key=$2; shift 2;;
+    -t|--dns_type) dns_type=$2; shift 2;;
+    -i|--dns_id) dns_id=$2; shift 2;;
+    -k|--dns_key) dns_key=$2; shift 2;;
+    -s|--dns_secret) dns_secret=$2; shift 2;;
     --) shift ; break ;;
     *) echo "getopt error!"; break ;;
   esac
