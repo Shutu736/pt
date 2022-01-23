caa33fa0c28f2e0cfe873132f4ca39fb8e575029
shutu <996822362@qq.com>
2022-01-23 21:42:27 +0800

    test

--- script/seedbox.sh
+++ script/seedbox.sh
@@ -5,11 +5,27 @@
 # Date:                 2022-1-23
 # Description:          Debian11 专用一键脚本
 
+# options
+args=`getopt -o u:p:d:t:i:k:s: -al username:,password:,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
+eval set -- "$args"
+
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u|--username) username=$2; shift 2;;
+    -p|--password) password=$2; shift 2;;
+    -d|--domain) domain=$2; shift 2;;
+    -t|--dns_type) dns_type=$2; shift 2;;
+    -i|--dns_id) dns_id=$2; shift 2;;
+    -k|--dns_key) dns_key=$2; shift 2;;
+    -s|--dns_secret) dns_secret=$2; shift 2;;
+    --) shift ; break ;;
+    *) echo "getopt error!"; break ;;
+  esac
+done
+
 echo $username
 echo $password
-
-# options
-source <(wget -qO- https://raw.githubusercontent.com/Shutu736/pt/master/script/options.sh)
 
 # create user
 if [[ ! -d "/home/$username" ]]; then
