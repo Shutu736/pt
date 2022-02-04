d97aa5534552e41c7669335bf339deacb25242fa
shutu <996822362@qq.com>
2022-02-04 10:05:00 +0800

    1111111

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
-args=`getopt -o u:p:w:x:d:t:i:k:s: -al username:,password:,webport:,port,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
+args=`getopt -o u:p:w:x:d:t:i:k:s: -al username:,password:,webport:,port:,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
 eval set -- "$args"
 
 while [ -n "$1" ]
