f880ee5df9ee8376f9378069918c30c297a5b8a7
shutu <996822362@qq.com>
2022-02-04 09:55:47 +0800

    11

--- script/seedbox.sh
+++ script/seedbox.sh
@@ -6,21 +6,21 @@
 # Description:          Debian11 专用一键脚本
 
 # options
-args=`getopt -al username:,password:,webport:,port:,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
+args=`getopt -o u:p:w:x:d:t:i:k:s: -al username:,password:,webport:,port,domain:,dns_type:,dns_id:,dns_key:,dns_secret: -n 'seedbox.sh' -- "$@"`
 eval set -- "$args"
 
 while [ -n "$1" ]
 do
   case "$1" in
-    --username) username=$2; shift 2;;
-    --password) password=$2; shift 2;;
-    --domain) domain=$2; shift 2;;
-    --webport) webport=$2; shift 2;;
-    --port) port=$2; shift 2;;
-    --dns_type) dns_type=$2; shift 2;;
-    --dns_id) dns_id=$2; shift 2;;
-    --dns_key) dns_key=$2; shift 2;;
-    --dns_secret) dns_secret=$2; shift 2;;
+    -u|--username) username=$2; shift 2;;
+    -p|--password) password=$2; shift 2;;
+    -w|--webport) webport=$2; shift 2;;
+    -x|--port) port=$2; shift 2;;
+    -d|--domain) domain=$2; shift 2;;
+    -t|--dns_type) dns_type=$2; shift 2;;
+    -i|--dns_id) dns_id=$2; shift 2;;
+    -k|--dns_key) dns_key=$2; shift 2;;
+    -s|--dns_secret) dns_secret=$2; shift 2;;
     --) shift ; break ;;
     *) echo "getopt error!"; break ;;
   esac
