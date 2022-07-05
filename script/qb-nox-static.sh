b5c794eba6766f4a6dc14c2b7d35ed60b2da2dc9
shutu <996822362@qq.com>
2022-07-05 23:46:03 +0800

    add 4.3.9

--- script/qb-nox-static.sh
+++ script/qb-nox-static.sh
@@ -22,7 +22,8 @@ fi
 versions[0]=qb-nox-static-419-lt1114
 versions[1]=qb-nox-static-419-lt1114-ax41
 versions[2]=qb-nox-static-438-lt1214
-j=3
+versions[3]=qb-nox-static-439-lt1215
+j=4
 
 for ((i = 0; i < j; i++)); do
   echo -e "\033[35m ${i}) ${versions[$i]}\033[0m"
