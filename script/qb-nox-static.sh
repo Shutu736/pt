0055f1e5610757b2e382a3867eaa044daa3079ac
shutu <996822362@qq.com>
2022-07-07 09:36:46 +0800

    12g

--- script/qb-nox-static.sh
+++ script/qb-nox-static.sh
@@ -22,9 +22,10 @@ fi
 versions[0]=qb-nox-static-419-lt1114
 versions[1]=qb-nox-static-419-lt1114-ax41
 versions[2]=qb-nox-static-419-lt1114-ln
-versions[3]=qb-nox-static-438-lt1214
-versions[4]=qb-nox-static-439-lt1215
-j=5
+versions[3]=qb-nox-static-419-lt1114-12g
+versions[4]=qb-nox-static-438-lt1214
+versions[5]=qb-nox-static-439-lt1215
+j=6
 
 for ((i = 0; i < j; i++)); do
   echo -e "\033[35m ${i}) ${versions[$i]}\033[0m"
